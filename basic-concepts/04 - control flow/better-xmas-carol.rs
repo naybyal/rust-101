2c912aa7784367c40bf229ab7f1c247bdec9baa4
naybyal <officiallynabiel@gmail.com>
2024-07-28 23:49:28 +0530

    12 Days of Christmas with Improved Code

--- basic-concepts/04 - control flow/better-xmas-carol.rs
+++ basic-concepts/04 - control flow/better-xmas-carol.rs
@@ -1,18 +1,10 @@
 fn main() {
     let days = [
-        "first", "second", "third",
-        "fourth", "fifth", "sixth",
-        "seventh", "eighth", "ninth",
-        "tenth", "eleventh", "twelfth"
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
     ];
-
     let stanzas = [
-        "a partridge in a pear tree", "two turtle doves,", "three French hens,", "four calling birds,",
-        "five golden rings,", "six geese a-laying,", "seven swans a-swimming,",
-        "eight maids a-milking,", "nine ladies dancing,", "ten lords a-leaping,",
-        "eleven pipers piping,", "twelve drummers drumming,"
+        "a partridge in a pear tree", "two turtle doves,", "three French hens,", "four calling birds,", "five golden rings,", "six geese a-laying,", "seven swans a-swimming,", "eight maids a-milking,", "nine ladies dancing,", "ten lords a-leaping,", "eleven pipers piping,", "twelve drummers drumming,"
     ];
-
     for day_index in 0..12 {
         println!("\nOn the {} day of Christmas,\nmy true love gave to me", days[day_index]);
         for stanza_index in (0..=day_index).rev() {
