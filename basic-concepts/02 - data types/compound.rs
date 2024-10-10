c8108618e976c6832e4165574e7d941e9e13cf90
naybyal <officiallynabiel@gmail.com>
2024-10-11 01:46:02 +0530

    Compound data types

--- basic-concepts/02 - data types/compound.rs
+++ basic-concepts/02 - data types/compound.rs
@@ -1,4 +1,9 @@
 fn main() {
     let numbers: [i32; 5] = [1,2,3,4,5];
     println!("{:?}", numbers);
+
+    let fruits: [&str; 2] = ["Apple", "Banana"];
+    println!("{:?}",fruits);
+
+    //  tuples
 }
