1f75960bf6b771e840d982808b2d36fe21cd60d1
naybyal <officiallynabiel@gmail.com>
2024-12-05 20:02:19 +0530

    functions: gcd of 2 numbers

--- basic-concepts/03 - functions/main.rs
+++ basic-concepts/03 - functions/main.rs
@@ -1,13 +1,32 @@
-fn f(x: i32) -> i32 { x + 1 }
+// fn f(x: i32) -> i32 { x + 1 }
+//
+// fn main() {
+//
+//     println!("{}", f({
+//
+//         let y = 1;
+//
+//         y + 1
+//
+//     }));
+//
+// }
+
+// gcd
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 &&  m != 0); // handling edge cases
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n   //  return n
+}
+
 
 fn main() {
-
-    println!("{}", f({
-
-        let y = 1;
-
-        y + 1
-
-    }));
-
+    println!("{}", gcd(50,25));
 }
