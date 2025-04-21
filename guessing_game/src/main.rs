12c730d93b2173ac2629a6796e43a78783f7b22e
Nabiel <132735141+naybyal@users.noreply.github.com>
2025-04-21 22:02:12 +0530

    Update main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -19,7 +19,6 @@ fn main() {
             };
 
         println!("You guessed: {guess}");
-
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
